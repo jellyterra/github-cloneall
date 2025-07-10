f607233a767adfb8cca54c50289b277106041cd0
Jelly Terra <jellyterra@proton.me>
2025-07-10 17:37:43 +0800

    Support `[baseurl, deep_clone, retry]`

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Jelly Terra
+// Copyright 2025 Jelly Terra <jellyterra@proton.me>
 // Use of this source code form is governed under the MIT License.
 
 package main
@@ -11,21 +11,35 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func main() {
 	err := _main()
 	if err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
+		fmt.Println(err)
 	}
 }
 
 func _main() error {
-	username := flag.String("u", "", "GitHub username")
+	var (
+		cloneBaseUrl = flag.String("h", "https://github.com", "Clone base URL.")
+		username     = flag.String("u", "", "GitHub username.")
+		deepClone    = flag.Bool("deep", false, "Deeply clone.")
+
+		retryDelay   = flag.Int("d", 3, "Retry delay.")
+		retryUplimit = flag.Int("r", 3, "Retry uplimit.")
+
+		gitArgs []string
+	)
 	flag.Parse()
 
+	if !*deepClone {
+		gitArgs = append(gitArgs, "--depth", "1")
+	}
+
 	if *username == "" {
-		println("missing username")
+		fmt.Println("Missing username.")
 		return nil
 	}
 
@@ -40,14 +54,38 @@ func _main() error {
 	if err != nil {
 		return err
 	}
-
-	_ = json.Unmarshal(respBody, &repos)
+	err = json.Unmarshal(respBody, &repos)
+	if err != nil {
+		return err
+	}
 
 	for _, r := range repos {
-		println("Cloning", r.FullName)
-		err = exec.Command("git", "clone", r.CloneUrl).Run()
-		if err != nil {
-			return err
+		fmt.Println(r.FullName)
+	}
+	reposLen := len(repos)
+
+	for i, r := range repos {
+		cloneUrl := *cloneBaseUrl + "/" + r.FullName
+
+		_, err := os.Stat(r.Name)
+		if !os.IsNotExist(err) {
+			continue
+		}
+
+		for retry := 0; retry != *retryUplimit; retry++ {
+			fmt.Println("\n>>> [", i+1, "/", reposLen, "] (", retry, "/", *retryUplimit, ") Cloning", cloneUrl)
+
+			cmd := exec.Command("git", append([]string{"clone", cloneUrl}, gitArgs...)...)
+			cmd.Stderr = os.Stderr
+			cmd.Stdout = os.Stdout
+
+			err = cmd.Run()
+			if err != nil {
+				time.Sleep(time.Duration(*retryDelay) * time.Second)
+				continue
+			}
+
+			break
 		}
 	}
 
